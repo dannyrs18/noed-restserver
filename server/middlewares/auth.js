573e1be7be8a500ec6a1ceb66a6b0eaaa65ecaab
Danny Romero <dannyors18@gmail.com>
2019-07-01 16:15:53 -0500

    Cambios

---
+++ server/middlewares/auth.js
@@ -0,0 +1,49 @@
+/**
+ * ===============================
+ * Test Token
+ * ===============================
+ */
+
+const jwt = require('jsonwebtoken');
+
+let verifyToken = (req, res, next) => {
+    let token = req.get('token');
+    // res.json({
+    //     token
+    // });
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: 'Token invalid'
+            });
+        }
+        req.usuario = decoded.usuario;
+        next()
+    }) 
+}
+
+/**
+ * ===============================
+ * Test ROLE_ADMIN
+ * ===============================
+ */
+
+let verifyAdmin = (req, res, next) => {
+
+    // Tiene q pasar primero por verifyToken para q usuario exista en req
+    let usuario = req.usuario;
+
+    if (usuario.rol !== 'ADMIN_ROLE') {
+        return res.json({
+            ok: false,
+            message: 'You are not an administrator user'
+        })
+    }
+    next()
+}
+
+module.exports = {
+    verifyToken,
+    verifyAdmin
+}
