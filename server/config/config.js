996e5a8c18f75d8c8cde11d137bfe344e0a275d1
Danny Romero <dannyors18@gmail.com>
2019-06-30 20:41:40 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,6 +17,10 @@ process.env.PORT = process.env.PORT || 3000;
 
 // creamos una variable global y colocaremos la configuración de la base de datos
 // si existe la propiedad process.env.NODE_ENV significa que estamos en produccion (HEROKU) caso contrario estamos en local y dependiendo de ello creamos la conexión
-process.env.DB_CONNECT = (!process.env.NODE_ENV) ? 
-    'mongodb://localhost:27017/cafe' : 
-    'mongodb+srv://dannyrs:8hQ8UyNSol0OmYwg@cluster0-doka9.mongodb.net/cafe'
+// process.env.DB_CONNECT = (!process.env.NODE_ENV) ? 
+//     'mongodb://localhost:27017/cafe' : 
+//     process.env.MONGO_URI_CONNECT
+// CODIGO ANTIGUO
+
+// Se creo una variable de entono dentro de heroku llamada MONGO_URI_CONNECT donde mantiene la direccion de conexión de la base de datos con sus credenciales
+process.env.MONGO_URI_CONNECT = process.env.MONGO_URI_CONNECT || 'mongodb://localhost:27017/cafe'
